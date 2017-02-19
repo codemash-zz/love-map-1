70050b7d080c3a5fbba71e024f9795be4c2ffdd1
topwood <869221472@qq.com>
2017-02-19 13:30:18 +0800

    first commit

---
+++ js/mapbox_feature_geojson.js
@@ -0,0 +1,93 @@
+var geojson = [
+{
+  "type": "FeatureCollection",
+  "features": [
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk01a7p1",
+		  "title": "南京 — 你在这",
+		  "details": "南京，你上学的地方，中山陵，玄武湖，鸡鸣寺，都是我们去过的地方",
+		  "description": "<img src='http://olluewtei.bkt.clouddn.com/P70101-105653.jpg' alt='forbunny'>元旦，第一次相见，你说你不漂亮，我说不错呀",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "college"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  118.7879840000,
+		  32.0601160000
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "5595e5482935805cc25660574669bf86"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk06d3h3",
+		  "title": "西安 — 我在这",
+		  "details": "西安，我待了七年的地方，带你去过我的新校区，带你去过城墙、大雁塔",
+		  "description": "<img src='http://olluewtei.bkt.clouddn.com/P70215-154403.jpg' alt='forbunny'>你为我翻山越岭，千山万水来到西安，心里很感动",
+      "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "college"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  108.929443,
+		  34.375178
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "4550dbd23b2eac654860ec82dfb2eb79"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk06d3h3",
+		  "title": "杭州 - 那年最后一个寒假一起游玩",
+		  "details": "杭州，寒假刚开始的时候我们相约西湖之滨",
+		  "description": "<img src='http://olluewtei.bkt.clouddn.com/P70121-160709.jpg' alt='forbunny'>虽然时间短暂，但是心里很温暖，世界那么大，我们要一起去看看",
+      "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "warehouse"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  120.1679070000,
+      30.2742840000
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "4550dbd23b2eac654860ec82dfb2eb79"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "title": "你我之间的连线",
+		  "details": "你我之间的连线",
+		  "description": "1000多公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+		  "stroke": "#1087bf",
+		  "stroke-width": 4,
+		  "stroke-opacity": 1
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  [
+      118.7879840000,
+  		32.0601160000
+		  ],
+		  [
+		  108.929443,
+		  34.375178
+		  ]
+		  ],
+		  "type": "LineString"
+	  },
+	  "id": "63150d05569345f5864e98bf255f7c6c"
+  }
+  ],
+  "id": "v4if.199afpm4"
+}
+];
